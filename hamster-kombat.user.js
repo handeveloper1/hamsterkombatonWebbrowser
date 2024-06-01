0ec9a90eb133ec8d745c706ae60ad5dc8edc98a8
Han.dev <36090819+oguzhanyazman@users.noreply.github.com>
2024-06-02 02:38:11 +0300

    Update hamster-kombat.user.js

--- hamster-kombat.user.js
+++ hamster-kombat.user.js
@@ -14,21 +14,16 @@
 (function() {
     'use strict';
 
-    // Функция для замены URL скрипта
+
     function replaceScriptUrl() {
-        // Список URL-адресов для замены
         const urlsToReplace = [
             'https://hamsterkombat.io/js/telegram-web-app.js',
             'https://app.hamsterkombat.io/js/telegram-web-app.js'
         ];
         const newUrl = 'https://ktnff.tech/hamsterkombat/telegram-web-app.js';
-
-        // Получаем все теги <script> на странице
         const scripts = document.getElementsByTagName('script');
         for (let script of scripts) {
-            // Проверяем, содержит ли src один из URL-адресов для замены
             if (urlsToReplace.includes(script.src)) {
-                // Создаем новый тег <script> с новым URL
                 const newScript = document.createElement('script');
                 newScript.src = newUrl;
                 newScript.type = 'text/javascript';
@@ -39,8 +34,6 @@
             }
         }
     }
-
-    // Наблюдатель за изменениями в DOM
     const observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.addedNodes.length) {
@@ -48,16 +41,10 @@
             }
         });
     });
-
-    // Настройки наблюдателя
     const config = {
         childList: true,
         subtree: true
     };
-
-    // Начинаем наблюдение за изменениями в DOM
     observer.observe(document.body, config);
-
-    // Первоначальный запуск замены URL
     replaceScriptUrl();
 })();
